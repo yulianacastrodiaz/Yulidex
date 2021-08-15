4441ab439835340502b08cbad09695a48239a196
yulianacastro7 <yulianacastrodiaz7@gmail.com>
2021-08-15 15:00:44 -0500

    mactualizacion de atributos

--- api/src/models/Type.js
+++ api/src/models/Type.js
@@ -2,13 +2,10 @@ const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('tipo', {
-    id: {
-      type: DataTypes.UUID,
-      primaryKey: true,
-    },
-    nombre: {
+  sequelize.define('type', {
+    name: {
       type: DataTypes.STRING,
+      unique: true,
     }
   });
 };
