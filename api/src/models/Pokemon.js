79e2c461904b7b2e4a845ef6ad434bd187ededd8
yulianacastro7 <yulianacastrodiaz7@gmail.com>
2021-08-14 20:40:28 -0500

    Creando modelo Pokemon con algunas validaciones

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -1,12 +1,68 @@
 const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
+
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
-    name: {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+    },
+    nombre: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true,
+      isInt: {
+        msg: "El nombre debería ser único. Este nombre ya existe."
+      },
+      validate: {
+        notNull: {
+          msg: 'Por favor ingrese un nombre.'
+        }
+      }
+    },
+    vida: {
+      type: DataTypes.INTEGER,
+      validate:{
+        isNumeric: true,
+        isInt: true,
+      }
+    },
+    fuerza: {
+      type: DataTypes.INTEGER,
+      validate:{
+        isNumeric: true,
+        isInt: true,
+      }
+    },
+    defensa: {
+      type: DataTypes.INTEGER,
+      validate:{
+        isNumeric: true,
+        isInt: true,
+      }
+    },
+    velocidad: {
+      type: DataTypes.INTEGER,
+      validate:{
+        isNumeric: true,
+        isInt: true,
+      }
+    },
+    altura: {
+      type: DataTypes.FLOAT,
+      validate:{
+        isNumeric: true,
+        isFloat: true,
+      }
+    },
+    fuerza: {
+      type: DataTypes.FLOAT,validate:{
+        isNumeric: true,
+        isFloat: true,
+      }
     },
   });
 };
