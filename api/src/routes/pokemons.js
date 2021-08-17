00453a5a74f2fcf96c0cb20db2e6d68d608b3fca
yulianacastro7 <yulianacastrodiaz7@gmail.com>
2021-08-17 09:14:15 -0500

    terminando ruta /:idpokemon

--- api/src/routes/pokemons.js
+++ api/src/routes/pokemons.js
@@ -7,20 +7,34 @@ const router = Router();
 
 router.get("/", (req, res) => {
   try {
-    axios.get('https://pokeapi.co/api/v2/pokemon')
-      .then(response => console.log(response))
-      // .then((results) => {console.log(results)});
+    fetch('https://pokeapi.co/api/v2/pokemon')
+      .then(response => response.json())
+      .then(data => res.json(data))
   } catch (error) {
-    console.log(error)
+    res.json(error)
   }
 })
-const apiKey = '4ae2636d8dfbdc3044bede63951a019b';
-const ciudad = 'londres'
-router.get("/:idPokemon", (req, res) => {
-  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric`)
-    .then(r => r.json())
-    .then(re => console.log(re))
-    res.send(req.params.idPokemon)
+
+router.get("/:idPokemon", async (req, res) => {
+  try {  
+    let ID = req.params.idPokemon;
+    if (ID.includes("-")) {
+      let pokemon = await Pokemon.findByPk(ID)
+      res.json(pokemon) 
+    } else {
+      let response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${ID}`)
+      const { name, types, id, sprites, weight, height, stats} = response.data;
+      const hp = stats[0].base_stat;
+      const attack = stats[1].base_stat;
+      const defense = stats[2].base_stat;
+      const speed = stats[5].base_stat;
+      const img = sprites.other.dream_world.front_default;
+      const tipos = types.map(slot  => slot.type.name);
+      res.json({name, tipos, id, img, weight, height, hp, attack, defense, speed })
+    }
+  } catch (error) {
+    res.status(404).json(error)
+  }
 })
 
 module.exports = router;
