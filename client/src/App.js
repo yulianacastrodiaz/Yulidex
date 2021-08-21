c9bbcb071054ebb6125c5fb2b1c9d25fcbde128f
yulianacastro7 <yulianacastrodiaz7@gmail.com>
2021-08-21 18:30:20 -0500

    App enrutada

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import React from 'react';
+import NavBar from "./components/navbar/NavBar.jsx";
+import { Route } from "react-router-dom";
+import LandingPage from './components/landingpage/LandingPage';
+import Home from './components/home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Pokemon</h1>
-    </div>
+    <React.Fragment>
+      <NavBar/>
+      <Route exact path='/home' component={Home}/>
+      <Route exact path='/' component={LandingPage}/>
+    </React.Fragment>
   );
 }
 
