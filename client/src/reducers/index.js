6a03705f4692950befb57a7a5f7efd5a0548da61
yulianacastro7 <yulianacastrodiaz7@gmail.com>
2021-08-24 10:36:36 -0500

    Confiugurando reducer

---
+++ client/src/reducers/index.js
@@ -0,0 +1,14 @@
+const initialState = {
+  pokemons: [],
+  types: [],
+  pokemonDetail: {}
+};
+
+export default function rootReducer(state = initialState, action) {
+  if(action.type === "GET_POKEMONS"){
+    return{
+      ...state,
+      pokemons: action.pokemons
+    }
+  }
+}
