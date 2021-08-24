01d8a67281e9c07626765c936567ebaea45bec6b
yulianacastro7 <yulianacastrodiaz7@gmail.com>
2021-08-24 16:11:01 -0500

    Renderizando info para pokemons

--- client/src/components/pokemon/Pokemon.jsx
+++ client/src/components/pokemon/Pokemon.jsx
@@ -1,40 +1,60 @@
 import React from "react";
 import s from "./Pokemon.module.css"
 
-export default function Pokemon (){
+const iconsTypes = {
+  "normal": "../img/types_icon/normal.svg",
+  "fighting": "../img/types_icon/fight.svg",
+  "flying": "../img/types_icon/flying.svg",
+  "poison": "../img/types_icon/poison.svg",
+  "ground": "../img/types_icon/ground.svg",
+  "bug": "../img/types_icon/bug.svg",
+  "ghost": "../img/types_icon/ghost.svg",
+  "rock": "../img/types_icon/rock.svg",
+  "fire": "../img/types_icon/fire.svg",
+  "grass": "../img/types_icon/grass.svg",
+  "steel": "../img/types_icon/steel.svg",
+  "psychic": "../img/types_icon/psychic.svg",
+  "ice": "../img/types_icon/ice.svg",
+  "dragon": "../img/types_icon/dragon.svg",
+  "water": "../img/types_icon/water.svg",
+  "electric": "../img/types_icon/electric.svg",
+  "dark": "../img/types_icon/dark.svg",
+  "fairy": "../img/types_icon/fairy.svg",
+}
+
+export default function Pokemon ({id, name, types, img, weight, height, attack}){
   return(
     <div className={s.pokemon}>
       <div className={s.back}>
         <div className={s.header}>
-          <p id={s.name}>BULBASAUR</p>
+          <p id={s.name}>{name.toUpperCase()}</p>
           <div className={s.id}>
             <img src="../img/pokeball.svg" alt="pokeball" height="30px"></img>
-            <p id={s.idnum}>1</p>
+            <p id={s.idnum}>{id}</p>
           </div>
         </div>
-        <img src="../img/bulbasaurimg.svg" alt="bulbasaur" height="130px" width="130px" id={s.img}></img>
+        <img src={img} alt={name} height="130px" width="130px" id={s.img}></img>
       </div>
       <div className={s.front}>
         <div className={s.about}>
           <div className={s.descriptionlarger}>
             <p>Height
-              <p>1</p>
+              <p>{height}</p>
             </p>
             <p>Type
               <div>
-                <img src="../img/pokebola.png" alt="prueba" height="30px"></img>
-                <img src="../img/pokebola.png" alt="prueba" height="30px"></img>
-                <img src="../img/pokebola.png" alt="prueba" height="30px"></img>
-
+                {types.map(t=>{
+                 return <img src={iconsTypes[t.name]} alt={`icono ${t.name}`} height="30px"></img>
+                })}
               </div>
             </p>
             <p>Weight
-              <p>69</p>
+              <p>{weight}</p>
             </p>
           </div>
           <div className={s.descriptionshort}>
             <p>Attack
-              <p>49</p>
+              <p>{attack}</p>
             </p>
             <p>Caught</p> 
           </div>
