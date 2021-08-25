3f93daf5543cea5070f6311bc991be763ca00210
yulianacastro7 <yulianacastrodiaz7@gmail.com>
2021-08-24 21:57:00 -0500

    LandingPage finalizada

--- client/src/components/landingpage/LandingPage.jsx
+++ client/src/components/landingpage/LandingPage.jsx
@@ -5,10 +5,12 @@ import s from "./LandingPage.module.css"
 export function LandingPage(){
   return(
     <div className={s.LandingPage}>
-      <img src="../img/option_1.png" id={s.pokemon} alt="" ></img>
-      {/* <NavLink to="/home">
+      <img src="../img/landingpage/bg_blob.svg" alt="blobs background" id={s.bg_blob}></img>
+      <img src="../img/landingpage/title_with_slogan.png" alt="titleandslogan" id={s.title}></img>
+      <img src="../img/landingpage/bg_gray.svg" alt="bg_gray" id={s.gray}></img>
+      <NavLink to="/home">
         <button id={s.go}>Go</button>
-      </NavLink> */}
+      </NavLink>
     </div> 
   )
 }
