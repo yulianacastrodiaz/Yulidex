e77d59d8eb903138f47618ebb17a3c0ae0c1e31e
yulianacastro7 <yulianacastrodiaz7@gmail.com>
2021-08-26 19:12:09 -0500

    componente home mejorado

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,50 +1,12 @@
 import React from "react";
 import Pokemons from "../Pokemons/Pokemons"
 import s from "./Home.module.css"
-import { connect } from "react-redux";
-import Pokemon from "../pokemon/Pokemon";
 
-export function Home({pokemons}){
+export default function Home(){
   return(
     <div className={s.home}>
-      <div className={s.buttons}>
-        <select name="Type">
-          <option>Type</option>
-        </select>
-        <select>
-          <option>Ascendant</option>
-        </select>
-        <select>
-          <option>Falling</option>
-        </select>
-      </div>
-      {
-        pokemons.length === 1 && pokemons[0].name !== undefined ? (
-          <div className={s.cardPokemon}>
-            <Pokemon
-            key={pokemons[0].id}
-            id={pokemons[0].id}
-            name={pokemons[0].name}
-            types={pokemons[0].types}
-            img={pokemons[0].img}
-            height={pokemons[0].height}
-            weight={pokemons[0].weight}
-            attack={pokemons[0].attack}
-            experience={pokemons[0].experience}
-            ></Pokemon>
-          </div>
-        ) : (
-          <Pokemons></Pokemons>
-        )
-      }
+        <Pokemons></Pokemons>
     </div>
   )
 }
 
-function mapStateToProps(state){
-  return{
-    pokemons: state.pokemons
-  }
-}
-
-export default connect(mapStateToProps, null)(Home);
