c4389b7d46ab7887f5e34e2501bc5818f0a6a096
yulianacastro7 <yulianacastrodiaz7@gmail.com>
2021-08-27 20:41:38 -0500

    Configuración de rutas /home, /home:id y /create

--- client/src/App.js
+++ client/src/App.js
@@ -5,15 +5,28 @@ import { Route } from "react-router-dom";
 import LandingPage from './components/landingpage/LandingPage';
 import Home from './components/home/Home';
 import NavBarout from './components/navbar/NavBarout'
+import PokemonDetail from './components/PokemonDetail/PokemonDetail';
+import CreatePokemon from './components/createPokemon/CreatePokemon';
+// import Pokemon from './components/pokemon/Pokemon';
+// import Pokemons from './components/Pokemons/Pokemons';
 
 function App() {
   return (
     <React.Fragment>
+      <Route exact path="/home/:id">
+        <NavBarout></NavBarout>
+        <PokemonDetail></PokemonDetail>
+      </Route>
       <Route exact path='/' component={NavBarin}/>
-      <Route path="/home" component={NavBarout}/>
-      <Route path="/create" component={NavBarout}/>
+      <Route path="/create">
+        <NavBarout></NavBarout>
+        <CreatePokemon></CreatePokemon>
+      </Route>
       <Route path="/types" component={NavBarout}/>
-      <Route path='/home' component={Home}/>
+      <Route exact path='/home'>
+        <NavBarout></NavBarout>
+        <Home></Home>
+      </Route>
       <Route exact path='/' component={LandingPage}/>
     </React.Fragment>
   );
