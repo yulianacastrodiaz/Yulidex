b10bf35b590b20a083a5bb5c17498d5ec8078f4d
yulianacastro7 <yulianacastrodiaz7@gmail.com>
2021-08-30 09:11:51 -0500

    Test backend sin terminar

--- api/tests/routes/pokemon.spec.js
+++ api/tests/routes/pokemon.spec.js
@@ -7,6 +7,10 @@ const { Pokemon, conn } = require('../../src/db.js');
 const agent = session(app);
 const pokemon = {
   name: 'Pikachu',
+  hp: 12,
+  attack: 30,
+  defense: 40,
+  speed: 45,
 };
 
 describe('Pokemon routes', () => {
@@ -17,8 +21,10 @@ describe('Pokemon routes', () => {
   beforeEach(() => Pokemon.sync({ force: true })
     .then(() => Pokemon.create(pokemon)));
   describe('GET /pokemons', () => {
-    xit('should get 200', () =>
+    it('should get 200', () =>
       agent.get('/pokemons').expect(200)
     );
+    it('debe buscar un pokemon por query', () => 
+      agent.get("/pokemons?name=pikachu").expect(200))
   });
 });
